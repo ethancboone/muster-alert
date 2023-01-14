eb05a075507eef21e9244a6fe95d2c2e63b1ea0a
ethancboone <89566274+ethancboone@users.noreply.github.com>
2023-01-14 14:25:52 -0600

    Removed some flake8 paramters

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from inbox_check import daily_inbox_check
 
 response = daily_inbox_check()
 
-if response != True: 
+if response is not True:
     print('You still need to muster')
 else:
     print('Muster completed')
